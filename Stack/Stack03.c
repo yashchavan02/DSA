3872b8e9bf4ec9cb8cc2a369590814c507486f0e
Yash Chavan <chavanyash127@gmail.com>
2024-07-04 23:52:36 +0530

    Stack using linkedlist in c

---
+++ Stack/Stack03.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct Node {
+    int Data;
+    struct Node* next;
+};
+
+bool IsEmpty(struct Node* Top) {
+    return Top == NULL;
+}
+
+bool IsFull() {
+    struct Node* ptr = (struct Node*)malloc(sizeof(struct Node));
+    bool full = ptr == NULL;
+    free(ptr); 
+    return full;
+}
+
+struct Node* Push(struct Node* Top, int value) {
+    if (IsFull()) {
+        printf("Stack Overflow! Cannot push %d\n", value);
+        return Top;
+    } else {
+        struct Node* ptr = (struct Node*)malloc(sizeof(struct Node));
+        ptr->Data = value;
+        ptr->next = Top;
+        Top = ptr;
+        return Top;
+    }
+}
+
+void PrintLinkedList(struct Node* ptr) {
+    while (ptr != NULL) {
+        printf("%d ", ptr->Data);
+        ptr = ptr->next;
+    }
+    printf("\n"); 
+}
+
+int main() {
+    struct Node* Top = NULL;
+
+    Top = Push(Top, 10);
+    Top = Push(Top, 11);
+    Top = Push(Top, 12);
+    Top = Push(Top, 13);
+    Top = Push(Top, 14);
+    Top = Push(Top, 15);
+    Top = Push(Top, 16);
+    Top = Push(Top, 17);
+    Top = Push(Top, 18);
+    Top = Push(Top, 19);
+    Top = Push(Top, 20);
+
+    PrintLinkedList(Top);
+
+    bool checkIsEmpty = IsEmpty(Top);
+
+    if(checkIsEmpty==0){
+        printf("Have some elements in linkedlist");
+    }
+    
+    return 0;
+}
