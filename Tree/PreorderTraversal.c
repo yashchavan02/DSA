5618ca7bb7c39a8497354077e540dfd5a1d71a03
Yash Chavan <chavanyash127@gmail.com>
2024-07-21 16:38:18 +0530

    Preorder traversal in binary tree in c

---
+++ Tree/PreorderTraversal.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    struct Node* left;
+    int Data;
+    struct Node* right;
+};
+
+void Preorder(struct Node* Root){
+    if(Root != NULL){
+    printf("%d ",Root->Data);
+    Preorder(Root->left);
+    Preorder(Root->right);
+    }
+}
+
+struct Node* newNode(int data) {
+    struct Node* T = (struct Node*)malloc(sizeof(struct Node));
+    if(T == NULL) {
+        printf("Memory Is Full.\n");
+        exit(0);
+    }
+    else {
+        T->left = T->right = NULL;
+        T->Data = data;
+        return T;
+    }
+  free(T);
+}
+
+int main(){
+
+struct Node* T0 = newNode(0);
+struct Node* T1 = newNode(1);
+struct Node* T2 = newNode(2);
+struct Node* T3 = newNode(3);
+struct Node* T4 = newNode(4);
+
+T0->left = T1;
+T1->left = T3;
+
+T0->right = T2;
+T1->right = T4;
+
+/*
+
+            0
+           /  \
+          1    2
+         /  \
+        3    4
+        
+    ans : 0 1 3 4 2
+
+*/
+
+   Preorder(T0);
+    return 0;
+}
