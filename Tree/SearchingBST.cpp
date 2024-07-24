ee756c3cf1e71a12c70a53c910c0d49986862613
Yash Chavan <chavanyash127@gmail.com>
2024-07-24 22:45:01 +0530

    Searching key in BST in cpp

---
+++ Tree/SearchingBST.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    struct Node* left;
+    int Data;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* T = new struct Node;
+    if(T == NULL) {
+        cout<<"Memory Is Full."<<endl;
+        exit(0);
+    }
+    else {
+        T->left = T->right = NULL;
+        T->Data = data;
+        return T;
+    }
+  delete T;
+}
+
+bool Search(struct Node* Root, int Key) {
+    if (Root == NULL){
+        return false;
+    } 
+    if (Root->Data == Key){
+        return true;
+    } 
+    else if (Key < Root->Data){
+        return Search(Root->left, Key);
+    } 
+    else{
+        return Search(Root->right, Key);
+    }
+}
+
+
+int main(){
+
+struct Node* T0 = newNode(4) ;   
+struct Node* T1 = newNode(2) ;   
+struct Node* T2 = newNode(5) ;   
+struct Node* T3 = newNode(1) ;   
+struct Node* T4 = newNode(3) ;
+
+T0->left = T1;
+T1->left = T3;
+
+T0->right = T2;
+T1->right = T4;
+
+/*
+
+        4  -- Root
+       / \
+      2   5 - Child Node Of Root
+     / \
+    1   3 --- Leaf Node
+
+*/ 
+
+if(Search(T0,4)){
+    cout<<"Element Is Present.";
+}
+else{
+    cout<<"Elements Is Not Present.";
+}
+   return 0;
+}
