d488d1238889fb7f7eecc85cecd6072d83662b18
Yash Chavan <chavanyash127@gmail.com>
2024-10-02 15:12:09 +0530

    update code

--- Heap/KthSmallestEle.cpp
+++ Heap/KthSmallestEle.cpp
@@ -5,12 +5,17 @@ using namespace std;
 int main(){
    int arr[] = {1,4,7,9,3,2,0};
    int size = sizeof(arr)/sizeof(arr[0]);
-   int i,k = 4;
+   int i,k = 1;
    priority_queue<int> p;
    for(i=0;i<k;i++){
       p.push(arr[i]);
    }
-   
-   cout<<endl;
+   for(i=k;i<size;i++){
+      if(p.top() > arr[i]){
+         p.pop();
+         p.push(arr[i]);
+      }
+   }
+   cout<<p.top()<<endl;
    return 0;
 }
